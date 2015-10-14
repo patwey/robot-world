19f7c4c74c6a8b22ad5440ca2ca9425da5b231ae
Pat Wey <patrickwey@gmail.com>
2015-10-14 09:05:31 -0600

    more routes!

--- app/controllers/robot_world_app.rb
+++ app/controllers/robot_world_app.rb
@@ -1,2 +1,44 @@
-class RobotWorldApp
+require 'models/robot_world'
+
+class RobotWorldApp < Sinatra::Base
+  set :root, File.expand_path("..", __dir__)
+  set :method_override, true
+
+  get '/' do
+    erb :dashboard
+  end
+
+  get '/robots' do
+    @robots = RobotWorld.all
+    erb :index
+  end
+
+  get '/robots/new' do
+    erb :new
+  end
+
+  post '/robots' do
+    RobotWorld.build(params[:robot])
+    redirect '/robots'
+  end
+
+  get '/robots/:id' do |id|
+    @robot = RobotWorld.find(id.to_i)
+    erb :show
+  end
+
+  get '/robots/:id/reprogram' do |id|
+    @robot = RobotWorld.find(id.to_i)
+    erb :edit
+  end
+
+  put '/robots/:id' do |id|
+    RobotWorld.reprogram(id.to_i, params[:robot])
+    redirect '/robots'
+  end
+
+  delete '/robots/:id' do |id|
+    RobotWorld.unplug(id.to_i)
+    redirect '/robots'
+  end
 end
