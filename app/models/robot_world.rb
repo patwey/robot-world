c339a3e890f6e55477a1b7265bb54ac38b9853b0
Pat Wey <patrickwey@gmail.com>
2015-10-15 16:12:37 -0600

    added self.statistics for dashboard stats

--- app/models/robot_world.rb
+++ app/models/robot_world.rb
@@ -24,6 +24,57 @@ class RobotWorld
     raw_robots.map { |data| Robot.new(data) }
   end
 
+  def self.statistics
+    stats = {}
+    robots = all
+
+    # average robot age
+    birthdates = robots.map { |robot| robot.birthdate }
+    ages = birthdates.map {|date| Time.now.year - date.split('/').last.to_i }
+    stats[:avg_age] = ages.reduce(:+) / ages.count
+
+    # robots hired each year
+    years_hired = robots.map { |robot| robot.date_hired.split('/').last }
+    stats[:years_hired] = {}
+    years_hired.each do |year|
+      year = year.to_i
+      stats[:years_hired][year] = 0
+      robots.each do |robot|
+        stats[:years_hired][year] += 1 if year == robot.date_hired.split('/').last.to_i
+      end
+    end
+
+    # robots in each department
+    departments = robots.map { |robot| robot.department }
+    stats[:departments] = {}
+    departments.each do |dept|
+      stats[:departments][dept] = 0
+      robots.each do |robot|
+        stats[:departments][dept] += 1 if dept == robot.department
+      end
+    end
+
+    # robots in each city
+    cities = robots.map { |robot| robot.city }
+    stats[:cities] = {}
+    cities.each do |city|
+      stats[:cities][city] = 0
+      robots.each do |robot|
+        stats[:cities][city] += 1 if city == robot.city
+      end
+    end
+    # robots in each state
+    states = robots.map { |robot| robot.state }
+    stats[:states] = {}
+    states.each do |state|
+      stats[:states][state] = 0
+      robots.each do |robot|
+        stats[:states][state] += 1 if state == robot.state
+      end
+    end
+    stats
+  end
+
   def self.build(robot)
     database.transaction do
       database['robots'] ||= []
