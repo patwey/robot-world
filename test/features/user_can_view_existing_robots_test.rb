7d334d8f969a1a29d03b7f85cf8500cd76c38266
Pat Wey <patrickwey@gmail.com>
2015-10-14 21:21:37 -0600

    feature testing is all-complete-like for users viewin robots

--- test/features/user_can_view_existing_robots_test.rb
+++ test/features/user_can_view_existing_robots_test.rb
@@ -1,16 +1,49 @@
 require_relative '../test_helper'
 
 class ViewRobotsTest < FeatureTest
+  def build_new(num)
+    num.times { RobotWorld.build({ 'name'       => "name #{num}",
+                                   'city'       => "city #{num}",
+                                   'state'      => "state #{num}",
+                                   'avatar'     => "avatar #{num}",
+                                   'birthdate'  => "birthdate #{num}",
+                                   'date_hired' => "date_hired #{num}",
+                                   'department' => "department #{num}"}) }
+  end
   def test_user_can_view_all_robots
   # As a user, when I navigate to the robots index, then I can see all the
   # existing robots.
-    skip
+    build_new(3)
+    visit '/robots'
+    assert_equal '/robots', current_path
+    assert has_css?('ul')
+
+    within('ul') do
+      assert has_css?('#robot-1')
+      assert has_css?('#robot-2')
+      assert has_css?('#robot-3')
+    end
   end
 
   def test_user_can_view_a_specific_robots_information
   # As a user, when I navigate to the robots index, and I click on the link
   # of a robot's name, then I see the robot's information
-    skip
+    build_new(1)
+    visit '/robots'
+    assert_equal '/robots', current_path
+    assert has_css?('ul')
+
+    within('ul') do
+      assert has_css?('#robot-1')
+      click_link('name 1')
+    end
+
+    assert_equal '/robots/1', current_path
+    assert has_content?('City: city 1')
+    assert has_content?('State: state 1')
+    assert has_content?('Birthdate: birthdate 1')
+    assert has_content?('Date Hired: date_hired 1')
+    assert has_content?('Department: department 1')
   end
 
   def test_user_can_view_robots_statistics
